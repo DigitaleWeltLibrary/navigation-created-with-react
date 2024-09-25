8038f3f41f1bd7ee327098b10d4072b425c7102d
Anna Holzer <r.holzer089@gmail.com>
2024-09-25 11:19:16 +0200

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { About, Contact, Home, Product } from "./components/Pages/Pages";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./app.scss";
+import Nav from "./components/Nav/Nav";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
